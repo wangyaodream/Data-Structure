b6d57caa47d64b39141bea6d1f4e4b5a0b871538
wangyao <wangyao.dream@hotmail.com>
2024-07-18 23:35:22 +0800

    dive to recursion

---
+++ LinkedList/src/Solution3.java
@@ -0,0 +1,24 @@
+public class Solution3 {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null)
+            return null;
+        ListNode res = removeElements(head.next, val);
+        if (head.val == val)
+            // 删除头结点，返回后续计算出的链表并进行递归计算
+            return res;
+        else{
+            // 将之前的头链表和计算后的链表对接
+            head.next = res;
+            return head;
+        }
+    }
+
+    public static void main(String[] args){
+        int[] num s = {1, 2, 6, 3, 4, 6, 8};
+        ListNode head = new ListNode(nums);
+        System.out.println(head);
+
+        ListNode res = (new Solution3()).removeElements(head, 6);
+        System.out.println(res);
+    }
+}
