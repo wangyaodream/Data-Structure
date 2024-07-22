4c053569a4dac66192190c5e3d156f3250dca0df
wangyao <wangyao.dream@hotmail.com>
2024-07-22 22:22:55 +0800

    create bst container function

--- BST/src/BST.java
+++ BST/src/BST.java
@@ -29,34 +29,58 @@ public class BST<E extends Comparable<E>> {
     }
 
     public void add(E e){
-        if (root == null) {
-            root = new Node(e);
-            size ++;
-        }
-        else{
-            add(root, e);
-        }
+//        if (root == null) {
+//            root = new Node(e);
+//            size ++;
+//        }
+//        else{
+//            add(root, e);
+//        }
+        root = add(root, e);
 
     }
 
-    private void add(Node node, E e){
+    private Node add(Node node, E e){
 
-        if(e.equals(node.e)){
-            return;
-        }else if(e.compareTo(node.e) < 0 && node.left == null){
-            node.left = new Node(e);
+//        if(e.equals(node.e)){
+//            return;
+//        }else if(e.compareTo(node.e) < 0 && node.left == null){
+//            node.left = new Node(e);
+//            size ++;
+//            return;
+//        }else if(e.compareTo(node.e) > 0 && node.right == null){
+//            node.right = new Node(e);
+//            size ++;
+//            return;
+//        }
+        if (node == null){
             size ++;
-            return;
-        }else if(e.compareTo(node.e) > 0 && node.right == null){
-            node.right = new Node(e);
-            size ++;
-            return;
+            return new Node(e);
         }
 
-        if (e.compareTo(node.e) < 0)
-            add(node.left, e);
-        else
-            add(node.right, e);
 
+        if (e.compareTo(node.e) < 0)
+            node.left =  add(node.left, e);
+        else if (e.compareTo(node.e) > 0)
+            node.right = add(node.right, e);
+
+        return node;
+    }
+
+    public boolean contains(E e){
+        return containers(root, e);
+    }
+
+    private boolean containers(Node node, E e){
+
+        if (node == null)
+            return false;
+        if(e.compareTo(node.e) == 0)
+            return true;
+        else if(e.compareTo(node.e) < 0){
+            return containers(node.left, e);
+        }else{
+            return containers(node.right, e);
+        }
     }
 }
