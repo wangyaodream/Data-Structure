b7e37bee4cfa7ebbb230a6abc7bade607617d550
wangyao <wangyao.dream@hotmail.com>
2024-07-22 22:06:22 +0800

    bst add element

--- BST/src/BST.java
+++ BST/src/BST.java
@@ -27,4 +27,36 @@ public class BST<E extends Comparable<E>> {
     public boolean isEmpty() {
         return size == 0;
     }
+
+    public void add(E e){
+        if (root == null) {
+            root = new Node(e);
+            size ++;
+        }
+        else{
+            add(root, e);
+        }
+
+    }
+
+    private void add(Node node, E e){
+
+        if(e.equals(node.e)){
+            return;
+        }else if(e.compareTo(node.e) < 0 && node.left == null){
+            node.left = new Node(e);
+            size ++;
+            return;
+        }else if(e.compareTo(node.e) > 0 && node.right == null){
+            node.right = new Node(e);
+            size ++;
+            return;
+        }
+
+        if (e.compareTo(node.e) < 0)
+            add(node.left, e);
+        else
+            add(node.right, e);
+
+    }
 }
