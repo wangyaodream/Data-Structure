ac53566e31b89f41a0e7254c24c5203613af1544
wangyao <wangyao.dream@hotmail.com>
2024-08-01 19:21:26 +0800

    implement remove method in bst

--- BST/src/BST.java
+++ BST/src/BST.java
@@ -219,6 +219,48 @@ public class BST<E extends Comparable<E>> {
         node.right = removeMax(node.right);
         return node;
     }
+
+    public void remove(E e) {
+        remove(root, e);
+    }
+
+    private Node remove(Node node, E e) {
+
+        if (node == null)
+            return null;
+
+        if (e.compareTo(node.e) < 0) {
+            remove(node.left, e);
+            return node;
+        } else if (e.compareTo(node.e) > 0) {
+            remove(node.right, e);
+            return node;
+        } else {
+            // e.compareTo(node.e) == 0
+            if(node.left == null) {
+                Node rightNode = node.right;
+                node.right = null;
+                size --;
+                return rightNode;
+            }
+
+            if (node.right == null) {
+                Node leftNode = node.left;
+                node.left = null;
+                size --;
+                return leftNode;
+            }
+
+            // 找到比待删除节点大的最小的节点，用它代替待删除节点
+            Node successor = minimum(node.right);
+            successor.right = removeMin(node.right);
+            successor.left = node.left;
+
+            node.left = node.right = null;
+            
+            return successor;
+        }
+    }
     @Override
     public String toString() {
         StringBuilder res = new StringBuilder();
--- BST/src/Main.java
+++ BST/src/Main.java
@@ -1,16 +1,22 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         BST<Integer> bst = new BST<>();
-        int[] nums = {5, 3, 6, 2, 4, 8};
-        for(int num: nums)
-            bst.add(num);
+        Random random = new Random();
 
-//        bst.preOrder();
-//        System.out.println();
-//        bst.inOrder();
-//        System.out.println();
-//        bst.postOrder();
-        bst.levelOrder();
-        // System.out.println(bst);
+        int n = 1000;
+
+        for(int i = 0; i < n; i ++) {
+            bst.add(random.nextInt(10000));
+        }
+
+        ArrayList<Integer> nums = new ArrayList<>();
+        while(!bst.isEmpty()) {
+            nums.add(bst.removeMin());
+        }
+
+        System.out.println(nums);
     }
 }
