3e5b1c5315299265cc8b1fb332971f6b6d19fc63
wangyao <wangyao.dream@hotmail.com>
2024-08-15 19:39:42 +0800

    add method siftup and siftdown

--- MaxHeap/src/MaxHeap.java
+++ MaxHeap/src/MaxHeap.java
@@ -1,4 +1,4 @@
-public class MaxHeap<E extends Comparable<E> {
+public class MaxHeap<E extends Comparable<E>> {
 
     private Array<E> data;
 
@@ -32,6 +32,52 @@ public class MaxHeap<E extends Comparable<E> {
 
     private int rightChild(int index) {
         return index * 2 + 2;
+    }
+
+    // 向堆中添加元素
+    public void add(E e) {
+        data.addLast(e);
+        siftUp(data.getSize() - 1);
+    }
+
+    private void siftUp(int k) {
+        while (k > 0 && data.get(parent(k)).compareTo(data.get(k)) < 0) {
+            data.swap(k, parent(k));
+            k = parent(k);
+        }
+    }
+
+    // 查看堆中的最大元素
+    public E findMax() {
+        if (data.getSize() == 0)
+            throw new IllegalArgumentException("Can not findMax when heap is empty.");
+
+        return data.get(0);
+    }
+
+    // 取出堆中最大元素
+    public E extractMax() {
+        E ret = findMax();
+
+        data.swap(0, data.getSize() - 1);
+        data.removeLast();
+        siftDown(0);
+
+        return ret;
+    }
+
+    private void siftDown(int k) {
+        while (leftChild(k) < data.getSize()) {
+            int j = leftChild(k);
+            if (j + 1 < data.getSize() &&
+                    data.get(j + 1).compareTo(data.get(j)) > 0)
+                j = rightChild(k);
+            if (data.get(k).compareTo(data.get(j)) >= 0)
+                break;
+
+            data.swap(k, j);
+            k = j;
+        }
     }
 
     
