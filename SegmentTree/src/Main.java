6f0b15185236548f59f3343a049be0409aed9fa1
wangyao <wangyao.dream@hotmail.com>
2024-08-27 21:37:24 +0800

    create segment tree with recursion

--- SegmentTree/src/Main.java
+++ SegmentTree/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int a = 9;
+        System.out.println(a / 2);
     }
 }
--- SegmentTree/src/SegmentTree.java
+++ SegmentTree/src/SegmentTree.java
@@ -1,12 +1,33 @@
 public class SegmentTree<E> {
     private E[] tree;
     private E[] data;
-    public SegmentTree(E[] arr) {
+    private Merger<E> merger;
+    public SegmentTree(E[] arr, Merger<E> merger) {
+        this.merger = merger;
         data = (E[])new Object[arr.length];
         for(int i = 0;i < arr.length; i++)
             data[i] = arr[i];
 
         tree = (E[])new Object[4 * arr.length];
+        buildSegmentTree(0, 0, data.length - 1);
+    }
+
+    // 在treeIndex的位置创建表示区间[l...r]的线段树
+    private void buildSegmentTree(int treeIndex, int l, int r) {
+
+        if (l == r){
+            tree[treeIndex] = data[l];
+            return;
+        }
+
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+
+        int mid = l + (r - l) / 2;
+        buildSegmentTree(leftTreeIndex, l, mid);
+        buildSegmentTree(rightTreeIndex, mid + 1, r);
+
+        tree[treeIndex] = merger.merge(tree[leftTreeIndex], tree[rightTreeIndex]);
     }
 
     public int getSize() {
@@ -25,5 +46,22 @@ public class SegmentTree<E> {
 
     private int rightChild(int index) {
         return 2 * index + 2;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append('[');
+        for(int i = 0; i < tree.length; i ++){
+            if(tree[i] != null)
+                res.append(tree[i]);
+            else
+                res.append("null");
+
+            if (i != tree.length - 1)
+                res.append(", ");
+        }
+        res.append(']');
+        return res.toString();
     }
 }
