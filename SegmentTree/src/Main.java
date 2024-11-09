a1151e59c296e31132f3f347d63696f17f0dd1e1
wangyao <wangyao.dream@hotmail.com>
2024-11-09 15:44:06 +0800

    add test for segment tree

--- SegmentTree/src/Main.java
+++ SegmentTree/src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        Integer[] nums = {-2, 0, 3, -5, 2, -1};
 
-        System.out.println("push test");
+        SegmentTree<Integer> segTree = new SegmentTree<>(nums, new Merger<Integer>() {
+            @Override
+            public Integer merge(Integer a, Integer b) {
+                return a * b;
+            }
+
+        });
+        System.out.println(segTree);
+
     }
 }
