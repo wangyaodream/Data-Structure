95442e719bc4e0308f5f9c7c60e97b587286c4b6
wangyao <wangyao.dream@hotmail.com>
2024-11-09 16:36:29 +0800

    select element in segmenttree

--- SegmentTree/src/SegmentTree.java
+++ SegmentTree/src/SegmentTree.java
@@ -34,6 +34,31 @@ public class SegmentTree<E> {
         return data.length;
     }
 
+    public E query(int queryL, int queryR) {
+        if (queryL < 0 || queryL >= data.length || queryR < 0 || queryR >= data.length || queryL > queryR)
+            throw new IllegalArgumentException("Index is illegal.");
+        return query(0, 0, data.length - 1, queryL, queryR);
+    }
+
+    private E query(int treeIndex, int l, int r, int queryL, int queryR) {
+        if (l == queryL && r == queryR) {
+            return tree[treeIndex];
+        }
+
+        int mid = l + (r - l) / 2;
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+
+        if (queryL >= mid + 1)
+            return query(rightTreeIndex, mid + 1, r, queryL, queryR);
+        else if (queryR <= mid)
+            return query(leftTreeIndex, l, mid, queryL, queryR);
+        // queryL、queryR没有在节点的leftChild和rightChild
+        E leftResult = query(leftTreeIndex, l, mid, queryL, mid);
+        E rightResult = query(rightTreeIndex, mid + 1, r, mid + 1, queryR);
+        return merger.merge(leftResult, rightResult);
+    }
+
     public E get(int index) {
         if (index < 0 || index >= data.length)
             throw new IllegalArgumentException("Index is illegal.");
